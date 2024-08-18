7146a8b804cca789a24d4ccc7500bb3bcf555725
Wali Ullah <wali190939@gmail.com>
2024-08-18 18:19:33 +0600

    update port

--- app.js
+++ app.js
@@ -63,7 +63,8 @@ app.use((req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, function () {
   console.log("Server started on port " + port);
